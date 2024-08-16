52fb1627c543f0a39e8baf0f54d384b332c52e11
chcode <Chris.Hicks@franklincountyohio.gov>
2024-08-16 14:54:12 -0400

    loop update

--- Loops.py
+++ Loops.py
@@ -1,25 +1,24 @@
 import random
-# print("Can you name 8 friends of yours:")
-# f1 = input('Friend one:')
-# f2 = input('Friend two:')
-# f3 = input('Friend three:')
-# f4 = input('Friend four:')
-# f5 = input('Friend five:')
-# prompt = [f1,f2,f3,f4,f5]
-# print("Your 2024 best friend is",random.choice(prompt))
+print("Can you name 8 friends of yours:")
+f1 = input('Friend one:')
+f2 = input('Friend two:')
+f3 = input('Friend three:')
+f4 = input('Friend four:')
+f5 = input('Friend five:')
+prompt = [f1,f2,f3,f4,f5]
+print("Your 2024 best friend is",random.choice(prompt))
 
 
 
 people = []
-print(f"Input the names of 5 people:")
-for x in range(0,6):
- person = input()
- people.append(person)
+x = 0
+for x in range(0,5):
+    person = input(f"Input the name of person { x + 1}:")
+    people.append(person)
+    add = random.randint(0,5)
 
-index = random.randint(0,5)
-
-random_person = people[index]
-print("Here is the random person", random_person)
+random_person = people[add]
+print("Your favorite person is", random_person)
 
 
 
